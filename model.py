430b73039889b69b061804f1514d2534db65bcb2
Manisha Dharmik <manishadharmik7@gmail.com>
2025-11-14 17:52:27 +0530

    Fix: deleted spacy model

--- model.py
+++ model.py
@@ -1,17 +1,10 @@
 from sentence_transformers import SentenceTransformer, util
-import spacy
+
 import subprocess
 import sys
 
 # Load Sentence-Transformer model
 model = SentenceTransformer("all-MiniLM-L6-v2")
-
-# Load spaCy model (auto-download if missing)
-try:
-    nlp = spacy.load("en_core_web_sm")
-except OSError:
-    subprocess.run([sys.executable, "-m", "spacy", "download", "en_core_web_sm"])
-    nlp = spacy.load("en_core_web_sm")
 
 skill_keywords = [
     "python", "sql", "machine learning", "deep learning",
@@ -27,6 +20,7 @@ def get_similarity(resume_text, jd_text):
     emb2 = get_embedding(jd_text)
     score = util.cos_sim(emb1, emb2).item()
     return round(score * 100, 2)
+
 
 def extract_skills(text):
     text = text.lower()
